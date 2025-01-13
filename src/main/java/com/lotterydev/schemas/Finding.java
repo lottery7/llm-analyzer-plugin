2fe164277a4cc56d2bfb4ccda995b944db0945f1
Eugene Akimov <akimoveugeny.04@mail.ru>
2025-01-13 21:34:35 +0300

    Change schema in system-prompt.txt

--- src/main/java/com/lotterydev/schemas/Finding.java
+++ src/main/java/com/lotterydev/schemas/Finding.java
@@ -1,6 +1,5 @@
 package com.lotterydev.schemas;
 
-import com.google.gson.annotations.SerializedName;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,12 +7,7 @@ import lombok.Data;
 @Builder
 public class Finding {
     private final int cwe;
-
-    @SerializedName("start_line_number")
     private final int startLineNumber;
-
-    @SerializedName("end_line_number")
     private final int endLineNumber;
-
     private final String description;
 }
